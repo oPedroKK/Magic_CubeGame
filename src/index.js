f0018f4974b2198e370aa19e38b4523e5793cda0
oPedroKK <phbarbozafarias@gmail.com>
2024-02-06 21:26:59 -0300

    resolvendo o problema de deploy

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+import BrowserRouter from 'react-router-dom/BrowserRouter'
+
+ReactDOM.render((
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
-  </React.StrictMode>
-);
+  </BrowserRouter>
+));
 
